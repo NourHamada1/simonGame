374a6bcda8e9bd9481dd346cb2423bc5289ca51e
NourHamada1 <107066959+NourHamada1@users.noreply.github.com>
2024-12-17 01:47:35 +0200

    Add files via upload

---
+++ game.js
@@ -0,0 +1,88 @@
+var buttonColours = ["red", "green", "blue", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+var level = 0;
+var gameStarted = false;
+
+$(document).keydown(function () {
+  if (!gameStarted) {
+    gameStarted = true;
+    nextSequence();
+  }
+});
+
+$(document).click(function () {
+  if (!gameStarted) {
+    gameStarted = true;
+    nextSequence();
+  }
+});
+
+$(".btn").click(function () {
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+  checkAnswer(userClickedPattern.length - 1);
+});
+
+function nextSequence() {
+  userClickedPattern = [];
+
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $("h1").html("Level " + level);
+
+  $("#" + randomChosenColour)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+
+  playSound(randomChosenColour);
+  level++;
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColour) {
+  $("#" + currentColour).addClass("pressed");
+
+  setTimeout(function () {
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
+}
+
+function checkAnswer(currentLevel) {
+  if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+      console.log("success");
+    }
+  } else {
+    $("body").css("background-color", "red");
+
+    setTimeout(function () {
+      $("body").css("background-color", "");
+    }, 200);
+    console.log("wrong");
+    $("h1").html("Game Over, Press Any Key to Restart ");
+    gamePattern = [];
+    gameStarted = false;
+    level = 0;
+
+    $(document).keydown(function () {
+      if (!gameStarted) {
+        gameStarted = true;
+        nextSequence();
+      }
+    });
+  }
+}
